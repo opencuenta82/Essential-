3f7f489cec47327b75065f27e4e16387d0dc83bf
opencuenta82 <opencuenta82@gmail.com>
2025-06-19 00:27:44 -0500

    mmmmmmm

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -22,7 +22,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/additional">Confg Whatsapp</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
